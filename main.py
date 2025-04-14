5ff8d0e6d64d699e911bd7214f76436446052096
Mantas <51176389+dedeSkar@users.noreply.github.com>
2025-04-14 22:16:33 +0300

    Update main.py
    Added Quallys SSL Lab API scan

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from Scanners.VirusTotal import VT_Main
+from Scanners.ssllabs import main_sslLabs
 
-VTApiKey = ""
+VTApiKey = "changeme"
+sslLabs_email = "changeme@changeme.lt"
 
 website_url = input("Enter hostname of website :")
 
 print(VT_Main(VTApiKey, website_url))
+print(main_sslLabs(sslLabs_email, website_url))
