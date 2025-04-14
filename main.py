0cca110c57eb11b3df4d4640731ad6569e565455
Mantas <51176389+dedeSkar@users.noreply.github.com>
2025-04-14 23:17:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,22 @@ sslLabs_email = "changeme@changeme.lt"
 
 website_url = input("Enter hostname of website :")
 
-print(VT_Main(VTApiKey, website_url))
-print(main_sslLabs(sslLabs_email, website_url))
+VT_Data = VT_Main(VTApiKey, website_url)
+SSL_Data = main_sslLabs(sslLabs_email, website_url)
+
+Report = f"""
+
+Website Security analysis
+------------------------------------------------------------------
+Website Title: {VT_Data["title"]}
+IP Address: {SSL_Data["ip_address"]}
+Status Code: {VT_Data["status_code"]}
+SSL grade: {SSL_Data["grade"]}
+VirusTotal Last analysis date: {VT_Data["last_analysis_date"]}
+VirusTotal Community score: {VT_Data["community_score"]}
+VirusTotal Security vendors' analysis : {VT_Data["score_overall"]}
+VirusTotal Security vendors' that found it Malicious: {VT_Data["Scanners_that_found_it_malicious"]}
+
+"""
+
+print(Report)
